50298f42f1fd2ab39e2d3192fc4ad66f17791d52
Toriyama <s24c1136ww@s.chibakoudai.jp>
2025-12-30 02:48:59 +0900

    プログラムの著作権表示をToriyamasanの名前に変更

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,18 +1,24 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 Toriyamasan
+# SPDX-License-Identifier: GPL-3.0-only
+
 import rclpy
 from rclpy.node import Node
 from person_msgs.msg import Person
+import math
 
-class Listener(Node):
+class AngleConverter(Node):
     def __init__(self):
         super().__init__("listener")
-        self.sub = self.create_subscription(Person, "person", self.cb, 10)
+        self.sub = self.create_subscription(Person, "angle", self.cb, 10)
 
     def cb(self, msg):
-        self.get_logger().info("Listen: %s, age: %d" % (msg.name, msg.age))
+        rad = msg.age * (math.pi / 180)
+        self.get_logger().info('受信: %d deg -> 変換後: %.4f rad' % (msg.age, rad))
 
 def main():
     rclpy.init()
-    node = Listener()
+    node = AngleConverter()
     rclpy.spin(node)
 
 if __name__ == '__main__':
