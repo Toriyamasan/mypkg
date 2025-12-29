c1ea7f151f85e7251dd8af87fd6dbb1e0d96cec2
Toriyama <s24c1136ww@s.chibakoudai.jp>
2025-12-30 03:40:58 +0900

    実装: 角度をdegreeで送信するロジックに変更し、著作権表示を更新

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,24 +1,29 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 Toriyamasan
+# SPDX-License-Identifier: GPL-3.0-only
+
 import rclpy
 from rclpy.node import Node
 from person_msgs.msg import Person
 
-class Talker(Node):
+class DegreeTalker(Node):
     def __init__(self):
-        super().__init__("talker")
-        self.pub = self.create_publisher(Person, "person", 10)
-        self.create_timer(0.5, self.cb)
-        self.n = 0
+        super().__init__("talker") # ノード名はtalkerのままでOK
+        self.pub = self.create_publisher(Person, "angle", 10)
+        self.create_timer(1.0, self.cb)
+        self.deg = 0
 
     def cb(self):
         msg = Person()
-        msg.name = "上田隆一"
-        msg.age = self.n
+        msg.name = "degree"
+        msg.age = self.deg
         self.pub.publish(msg)
-        self.n += 1
+        self.get_logger().info('送信角度: %d deg' % msg.age)
+        self.deg = (self.deg + 15) % 360
 
 def main():
     rclpy.init()
-    node = Talker()
+    node = DegreeTalker()
     rclpy.spin(node)
 
 if __name__ == '__main__':
